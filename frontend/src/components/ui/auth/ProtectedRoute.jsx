38837a1ec2bb180aec48ff8f6a34a080da7cefeb
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-28 11:19:01 +0300

    Added protected route

--- frontend/src/components/ui/auth/ProtectedRoute.jsx
+++ frontend/src/components/ui/auth/ProtectedRoute.jsx
@@ -1,8 +1,48 @@
-import useAuthStore from '@/lib/store/authStore'
-import React from 'react'
+import api from "@/lib/api/apiClient";
+import useAuthStore from "@/lib/store/authStore";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import React, { useEffect } from "react";
+import { Navigate, useLocation } from "react-router";
 
-export const ProtectedRoute = ({children}) => {
-    const {token} = useAuthStore()
-    console.log('token', token);
-  return children
-}
+export const ProtectedRoute = ({ children }) => {
+  const {setAuth, clearAuth, token} = useAuthStore();
+
+  const location = useLocation();
+  const { data, error, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["currentUser"],
+    queryFn: async () => {
+      const response = await api.get("/auth/profile", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    },
+    enabled: !token,
+    retry: 1,
+  });
+
+  //   error case
+  useEffect(() => {
+    if (isError) {
+      clearAuth();
+    }
+  }, [isError, error, clearAuth]);
+  // success case
+  useEffect(() => {
+    setAuth(data, token);
+  }, [isSuccess, data, setAuth, token]);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+  if (isError) {
+    return <Navigate to={"/login"} state={{ from: location }} replace />;
+  }
+  return children;
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOption: {
@@ -19,6 +20,7 @@ createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
       <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
