aef88138f4296daa07d8679e4f86822ea3538471
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-14 18:18:43 +0300

    solving

--- backend/index.js
+++ backend/index.js
@@ -49,13 +49,14 @@ app.use("/api/delete", DeleteTask);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Server frontend in Production
+// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // serve the frontend app
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
